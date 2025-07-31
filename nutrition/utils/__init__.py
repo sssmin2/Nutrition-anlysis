1a047ad8d9b8312f3507f89e9b86b6c71ad004b6
sssmin2 <112750726+sssmin2@users.noreply.github.com>
2025-07-31 14:51:19 +0900

    Update __init__.py

--- nutrition/utils/__init__.py
+++ nutrition/utils/__init__.py
@@ -1,9 +1,11 @@
 from .nutrition_calculator import calculate_nutrition
 from .emphasis_rules import check_emphasis
+from .comment_generator import generate_comment
 
 __all__ = [
     "calculate_nutrition",
     "check_emphasis",
+    "generate_comment"
 ]
 
 def serialize_model(model):
